7dbf29a87a5f26df60ac32e07153792906ba7d8b
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2025-03-20 10:39:22 +0500

    hw02-generics

--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,20 +1,37 @@
 package homework;
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
 
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private TreeMap<Customer, String> treeMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        var customerEntry = treeMap.firstEntry();
+        var customer = customerEntry.getKey();
+        return getNewCustomer(customer, customerEntry);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        for (Map.Entry<Customer, String> entry: treeMap.entrySet()) {
+            Customer customerKey = entry.getKey();
+            if (customerKey.getScores() > customer.getScores()) {
+                return getNewCustomer(customerKey, entry);
+            }
+        }
+        return null;
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        treeMap.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> getNewCustomer(Customer customer, Map.Entry<Customer, String> entry) {
+        var newCustomer = new Customer(customer.getId(), customer.getName(), customer.getScores());
+        return new AbstractMap.SimpleEntry<>(newCustomer, entry.getValue());
+    }
 }
