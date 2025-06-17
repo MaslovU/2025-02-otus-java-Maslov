330d7c99b63d457ca29562b15a900854a5fe7881
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2025-06-17 19:25:15 +0500

    result

--- L16-serialization/build.gradle.kts
+++ L16-serialization/build.gradle.kts
@@ -9,11 +9,26 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("javax.json:javax.json-api:1.0")
+
+    implementation ("ch.qos.logback:logback-classic")
+    implementation ("com.google.guava:guava")
+    implementation ("com.google.code.gson:gson")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events ("passed", "skipped", "failed")
+    }
 }
--- L16-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,17 +1,39 @@
 package ru.otus.dataprocessor;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import ru.otus.exceptions.FileProcessException;
 import ru.otus.model.Measurement;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private final  List<Measurement> measurements;
+
+    private static final Type LIST_TYPE_MEASUREMENT = new TypeToken<List<Measurement>>(){}.getType();
+
+    public ResourcesFileLoader(String fileName) throws FileProcessException {
+        try {
+            var resource = ResourcesFileLoader.class.getClassLoader().getResource(fileName);
+            if (resource == null) {
+                throw new FileProcessException("File: '" + fileName + "' not found");
+            }
+            var jsonContent = Files.readString(Path.of(resource.toURI()));
+            this.measurements = new Gson().fromJson(jsonContent, LIST_TYPE_MEASUREMENT);
+        } catch (URISyntaxException | IOException e) {
+            throw new FileProcessException(e);
+        }
+    }
 
     @Override
     public List<Measurement> load() {
-        // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        //читает файл, парсит и возвращает результат
+        return measurements;
     }
 }
