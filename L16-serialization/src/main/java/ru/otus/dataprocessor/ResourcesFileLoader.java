9e3f926ee5c9dbaff8560bafd0d319afd3a34df3
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2025-06-18 14:27:27 +0500

    polish

--- L16-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -9,10 +9,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
