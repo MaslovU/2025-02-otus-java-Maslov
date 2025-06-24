1c809fbb12df2f693ba477051ba3fb971347d8cd
Маслов Юрий (Maslov Yurii) <37995469+MaslovU@users.noreply.github.com>
2025-06-24 19:45:24 +0500

    hw09-jdbs (#10)
    * hw09-jdbs
    
    * polish
    
    * hw09-jdbs
    
    * polish
    
    * polish
    
    * polish after review
    
    * fixed build
    
    * polish after review
    
    * polish after review
    
    ---------
    
    Co-authored-by: yuvmaslov <yuvmaslov@ekb.beeline.ru>
    Co-authored-by: yuvm <mjv13ech@gmail.com>

---
+++ L18-jdbc/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -0,0 +1,111 @@
+package ru.otus.jdbc.mapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.core.repository.DataTemplate;
+import ru.otus.core.repository.executor.DbExecutor;
+import ru.otus.exceptions.DataBaseOperationException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
+
+/** Сохратяет объект в базу, читает объект из базы */
+@SuppressWarnings("java:S1068")
+public class DataTemplateJdbc<T> implements DataTemplate<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(DataTemplateJdbc.class);
+    private final DbExecutor dbExecutor;
+    private final EntitySQLMetaData entitySQLMetaData;
+    private final EntityClassMetaData<T> entityClassMetaData;
+
+    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData, EntityClassMetaData<T> entityClassMetaData) {
+        this.dbExecutor = dbExecutor;
+        this.entitySQLMetaData = entitySQLMetaData;
+        this.entityClassMetaData = entityClassMetaData;
+    }
+
+    @Override
+    public Optional<T> findById(Connection connection, long id) {
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectByIdSql(), List.of(id), rs -> {
+            try {
+                if (rs.next()) {
+                    return mapToObject(rs);
+                }
+                return null;
+            } catch (SQLException e) {
+                throw new DataBaseOperationException(e);
+            }
+        });
+    }
+
+    @Override
+    public List<T> findAll(Connection connection) {
+        return dbExecutor.executeSelect(connection, entitySQLMetaData.getSelectAllSql(), List.of(), rs -> {
+            var list = new ArrayList<T>();
+            try {
+                while (rs.next()) {
+                    list.add(mapToObject(rs));
+                }
+                return list;
+            } catch (SQLException e) {
+                throw new DataBaseOperationException(e);
+            }
+        }).orElseThrow(() -> new RuntimeException("Unexpected error"));
+    }
+
+    @Override
+    public long insert(Connection connection, T client) {
+        return dbExecutor.executeStatement(
+                connection,
+                entitySQLMetaData.getInsertSql(),
+                getFieldValues(client, entityClassMetaData.getFieldsWithoutId())
+        );
+    }
+
+    @Override
+    public void update(Connection connection, T client) {
+        dbExecutor.executeStatement(connection, entitySQLMetaData.getUpdateSql(), getFieldValues(client, entityClassMetaData.getAllFields()));
+    }
+
+    private List<Object> getFieldValues(T object, List<Field> fields) {
+        return fields.stream()
+                .map(field -> {
+                    field.setAccessible(true);
+                    try {
+                        return field.get(object);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Field is unavailable");
+                    }
+                })
+                .collect(toList());
+    }
+
+    private T mapToObject(ResultSet resultSet) throws SQLException {
+        List<Object> entityFields = new ArrayList<>();
+        long idValue = resultSet.getLong(entityClassMetaData.getIdField().getName());
+        entityFields.add(idValue);
+        entityClassMetaData.getFieldsWithoutId().forEach(
+                f -> {
+                    try {
+                        entityFields.add(resultSet.getObject(f.getName()));
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+        try {
+            return entityClassMetaData.getConstructor().newInstance(entityFields.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            log.error("Got InstantiationException | IllegalAccessException | InvocationTargetException");
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
---
+++ L18-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -0,0 +1,82 @@
+package ru.otus.jdbc.mapper;
+
+import ru.otus.annotatons.Id;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static java.util.Objects.requireNonNull;
+
+public class EntityClassMetaDataImpl<T> implements EntityClassMetaData<T> {
+    private String entityClassName;
+    private Constructor<?> constructor;
+    private List<Field> allFields;
+    private List<Field> nonIdFields;
+    private Field idMarkedField;
+
+    public EntityClassMetaDataImpl(Class<T> entityClass) {
+        requireNonNull(entityClass);
+        defineClassName(entityClass);
+        defineFields(entityClass);
+        defineClassConstructor(entityClass);
+        defineAllFields(entityClass);
+    }
+
+    private void defineClassName(final Class<T> entityClass) {
+        this.entityClassName = entityClass.getSimpleName();
+    }
+
+    private void defineFields(final Class<T> entityClass) {
+        Field[] declaredFields = entityClass.getDeclaredFields();
+
+        idMarkedField = stream(declaredFields)
+                .filter(f -> f.isAnnotationPresent(Id.class))
+                .findAny().orElseThrow(() -> new RuntimeException("No fields marked as Id"));
+
+        nonIdFields = stream(declaredFields)
+                .filter(f -> !f.isAnnotationPresent(Id.class))
+                .collect(Collectors.toList());
+    }
+
+    private void defineClassConstructor(final Class<T> entityClass) {
+        Constructor<?>[] constructors = entityClass.getDeclaredConstructors();
+        this.constructor = stream(constructors)
+                .filter(e -> e.getParameterCount() == nonIdFields.size() + 1)
+                .findAny().orElseThrow(() -> new RuntimeException("Can't define correct persistence constructor"));
+    }
+
+    private void defineAllFields(final Class<T> entityClass) {
+        allFields = new ArrayList<>();
+        allFields.addAll(nonIdFields);
+        allFields.add(idMarkedField);
+    }
+
+    @Override
+    public String getName() {
+        return entityClassName;
+    }
+
+    @Override
+    public Constructor getConstructor() {
+        return constructor;
+    }
+
+    @Override
+    public Field getIdField() {
+        return idMarkedField;
+    }
+
+    @Override
+    public List<Field> getAllFields() {
+        return allFields;
+    }
+
+    @Override
+    public List<Field> getFieldsWithoutId() {
+        return this.nonIdFields;
+    }
+}
