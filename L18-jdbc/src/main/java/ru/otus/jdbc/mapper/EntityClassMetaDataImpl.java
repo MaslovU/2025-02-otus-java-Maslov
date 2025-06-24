5a873e55e20d1149526b41c57294fdf92b850ac3
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2025-06-24 19:45:12 +0500

    polish after review

--- L18-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
+++ L18-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
